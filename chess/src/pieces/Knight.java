4078e7f89d9de45ee28fd7e5d0a932c74eda597d
mehdi hossieni <39370153+meidix@users.noreply.github.com>
2018-10-30 18:11:05 +0330

    Knight updated

--- chess/src/pieces/Knight.java
+++ chess/src/pieces/Knight.java
@@ -13,15 +13,25 @@ public class Knight extends piece
     @Override
     public void move(Position pos)
     {
-
+        if(isThere(pos))
+        {
+            this.setPosition(pos.getX(), pos.getY());
+        }
     }
 
     @Override
     public boolean isThere(Position pos)
     {
-        if(Board.isEmpty(pos))
-            return false;
-        return true;
+        int x0 = this.position().getX(), y0 = this.position().getY(), x1 = pos.getX(), y1 = pos.getY();
+        if((x0 + 2 == x1 && y0 + 1 == y1) || (x0 + 1 == x1 && y0 + 2 == y1))
+        if(!Board.isEmpty(pos))
+            return true;
+        else
+        {
+            if(Board.get(pos.getX(), pos.getY()).getColour().equals(this.getColour()))
+                return false;
+            else return true;
+        }
     }
 
 }
