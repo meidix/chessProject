2421c7fffeed12ddacee90d2419b2627a0249d14
alibahreinian110 <44114504+alibahreinian110@users.noreply.github.com>
2018-11-03 22:53:18 +0330

    class Action

---
+++ chess/src/Action/Action.java
@@ -0,0 +1,35 @@
+package Action;
+
+import Board.Position;
+
+import java.util.*;
+
+public class Action {
+    private static Stack<Action> action = new Stack<Action>();
+    private Position startPosition;
+    private Position endPosition;
+    private boolean captures;
+
+    public Action() {}
+    public Action(Position sp, Position ep, boolean cap)
+    {
+        startPosition = sp;
+        endPosition = ep;
+        captures = cap;
+        action.push(this);
+    }
+
+    public Action undo()
+    {
+        return action.pop();
+    }
+
+    public Position getStartPosition() {return startPosition; }
+    public Position getEndPosition() { return endPosition; }
+    public Boolean captures() { return captures; }
+
+
+
+
+
+}
